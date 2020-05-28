d828e96b7fde24c2a10334705366fe8105f111e5
Hiroshige Negishi <hirokuma@globhcet.org>
2020-05-28 21:30:16 +0700

    update

--- src/constants/connection.ts
+++ src/constants/connection.ts
@@ -3,4 +3,6 @@ require('dotenv').config()
 const ENDPOINT = 'https://apne1-1101-nfr.sentinelone.net'
 const API_TOKEN = process.env.API_TOKEN
 
+console.log(API_TOKEN)
+
 export { ENDPOINT, API_TOKEN }
